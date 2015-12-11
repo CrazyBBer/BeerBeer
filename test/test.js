2e65fe4afd6752d26a371e8ee85bb61c3e78772b
Eamon <Chenlulei@live.com>
2015-12-11 13:46:06 +0800

    update test  project using ..

---
+++ test/test.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Administrator on 2015/12/11.
+ * Description  test files
+ */
+var should = require('should');
+var assert = require('assert');
+
+
+describe('Test Framework',function (){
+    it('should have mocha installed and running..',function (){
+        assert.equal(true,true);
+    } )
+
+    it('should have the should library installed and running for float testing',function(){
+
+        true.should.eql(true);
+    })
+
+} )
